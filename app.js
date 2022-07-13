dd8129e5e1b3eea467e1eb1c598252d48826f527
rafaelbchs <rafaelbchacins@gmail.com>
2022-07-13 14:22:34 -0500

    showed titles and names

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const app = express();
+const data = require("./postBank")
+const morgan = require('morgan')
+const {list, find} = data
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  const posts = list()
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title> Wizard News </title>
+  </head>
+  <body>
+  <ul>
+    ${posts.map(post=>`<li>${post.name}</li><li>${post.title}</li>`).join(' ')}
+  </ul>
+  </body>
+  `
+  res.send(html)
+  posts.map((post)=>{console.log(post.name)})
+}
+);
 
 const PORT = 1337;
 
